5da869a79ba2039f55b96cf63b6f753fe671b0ba
cthungIT <cthung.it2013@gmail.com>
2025-08-04 11:10:10 +0700

    debug

--- lib/shard_connect.rb
+++ lib/shard_connect.rb
@@ -3,14 +3,18 @@
 require 'active_record'
 require 'shard_connect/version'
 
-ActiveSupport.on_load(:active_record) do
-  require 'shard_connect/relation_proxy'
-  require 'shard_connect/connection_adapters'
-  # require 'shard_connect/connection_handling'
-  #   require 'shard_connect/current_shard_tracker'
-  #   require 'shard_connect/association_shard_check'
-  # require 'shard_connect/shared_persistence'
-  #   require 'shard_connect/association'
-  #   require 'shard_connect/log_subscriber'
-  require 'shard_connect/using_shard'
-end
+require 'shard_connect/relation_proxy'
+require 'shard_connect/connection_adapters'
+require 'shard_connect/using_shard'
+
+# ActiveSupport.on_load(:active_record) do
+#   require 'shard_connect/relation_proxy'
+#   require 'shard_connect/connection_adapters'
+#   # require 'shard_connect/connection_handling'
+#   #   require 'shard_connect/current_shard_tracker'
+#   #   require 'shard_connect/association_shard_check'
+#   # require 'shard_connect/shared_persistence'
+#   #   require 'shard_connect/association'
+#   #   require 'shard_connect/log_subscriber'
+#   require 'shard_connect/using_shard'
+# end
